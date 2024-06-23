f70a996cf84b07f3194d75718cfb33a553456aa0
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-06-23 12:14:21 -0400

    adds csharp color

--- theme/languages/csharp.ts
+++ theme/languages/csharp.ts
@@ -4,8 +4,10 @@ import { token } from '../utils'
 export const csharp = [
   // Match "=" from JSX for HTML attributes
   token('entity.name.type.cs', Colors.schemes[THEME].secondary, 'italic'),
+  token('entity.name.type.class.cs', Colors.schemes[THEME].secondary, 'bold'),
+
   token('storage.modifier.public.cs', Colors.schemes[THEME].primary),
-  token('variable.other.object.property.cs', Colors.schemes[THEME].primary),
-  token('entity.name.type.cs', Colors.schemes[THEME].primary),
+
   token('variable.language.this.cs', Colors.schemes[THEME].secondary, 'italic'),
+  token('variable.other.object.property.cs', Colors.schemes[THEME].primary),
 ]
